1f7c98d9cb847b18d359c5b5876e9c7252b0ed5a
AntonShumin <AntonShuminBe@gmail.com>
2016-07-14 23:36:04 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<html>
+<head>
+    <title>Relaxy</title>
+    <link rel="stylesheet" type="text/css" href="Media/css/layout.css">
+</head>
+<body>
+
+
+
+
+<div id="wrap_video">
+    <div id="video_box">
+        <div id = "video_overlay">
+            <img src="Media/Pictures/ColorOverlay.png"/>
+        </div>
+        <video id="sampleMovie" autoplay loop>
+            <!--
+            <source src="HTML5Sample_H264.pmov" />
+            <source src="HTML5Sample_Ogg.ogv" />
+            -->
+            <source src="Media/Video/Wellness.webm" />
+        </video>
+    </div>
+</div>
+</body>
+
+</html>
