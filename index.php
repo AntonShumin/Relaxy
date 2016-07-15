491249b1a0389063de25a39ee9820567dbbd1070
AntonShumin <AntonShuminBe@gmail.com>
2016-07-15 20:53:44 +0200

    html5 video centered

--- index.php
+++ index.php
@@ -1,28 +1,34 @@
-<?php
 
-?>
 <html>
 <head>
     <title>Relaxy</title>
     <link rel="stylesheet" type="text/css" href="Media/css/layout.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
-<div id="wrap_video">
-    <div id="video_box">
-        <div id = "video_overlay">
-            <img src="Media/Pictures/ColorOverlay.png"/>
-        </div>
+    <!--
+    <div id="upper_frame">
+        <img src="Media/Pictures/ColorOverlay.png"/>
 
     </div>
-</div>
-    <div id="front_upper_frame">
+    -->
+    <div class="background-wrap">
         <video id="sampleMovie" autoplay loop>
             <!--
             <source src="HTML5Sample_H264.pmov" />
             <source src="HTML5Sample_Ogg.ogv" />
             -->
-            <source src="Media/Video/Wellness.webm" />
+            <source src="Media/Video/Wellness_Small.webm" />
         </video>
+    </div>
+
+    <div class="content">
+
+        <h1>Video background tutorial</h1>
+        <p>this is some text</p>
+        </img>
+
+
     </div>
 
 
