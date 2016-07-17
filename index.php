1563c3c93d74b5f912d1abc248868d2374aca824
AntonShumin <AntonShuminBe@gmail.com>
2016-07-17 14:40:14 +0200

    Basic div layout frontpage

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
         <link rel="stylesheet" type="text/css" href="Media/css/layout.css">
     </head>
     <body>
-
+        <!-- --------------------------------------------
+        -----------------Background----------------------
+        --------------------------------------------- -->
         <div class="video_wrap">
             <video id="video" autoplay loop>
                 <!--
@@ -17,19 +19,50 @@
                 <source src="Media/Video/Wellness_Small.webm" />
             </video>
         </div>
-
+        <!-- --------------------------------------------
+        -----------------Upper Bar----------------------
+        --------------------------------------------- -->
         <div class="upper_bar border_bottom shadow_bottom">
-
+            <img src="Media/Pictures/logo_bloem.png"/>
         </div>
 
-        <div class="content">
-
-            <h1>Video background tutorial</h1>
-            <p>this is some text</p>
+        <div class="front_frame_1">
+            <img src="Media/Pictures/logo_bloem.png"/>
+            <h1> &nbsp SAUNA &nbsp WELLNESS &nbsp RELAXY &nbsp</h1>
+            <p>Zoek, boek en vergelijk meer dan 180 saunas in Vlaanderen.<br/>Uw perfecte avond begint bij ons.</p>
+            <p></p>
         </div>
 
         <div class="front_frame_2 shadow_top_left border_top">
-            <p>Test</p>
+            <h1>AANRADERS VAN DE WEEK</h1>
+
+        </div>
+
+        <div class="front_frame_3">
+            <h1>RELAXY</h1>
+            <img src="Media/Pictures/Flair_1.png" />
+            <ul>
+                <li>Meer dan 180 partners</li>
+                <li>Snel en overzichtelijk vergelijken</li>
+                <li>Actie en promotie tracker</li>
+                <li>Routebeschrijving</li>
+                <li>Openingsuren</li>
+                <li>Gebruikers reviews</li>
+                <li>Weerbericht</li>
+
+            </ul>
+        </div>
+
+        <div class="front_frame_2">
+            <h1>RELAXY BLOG</h1>
+        </div>
+
+        <div class="front_frame_3">
+            <h1>ONZE ANDERE AANBIEDINGEN</h1>
+        </div>
+
+        <div class="front_frame_2">
+           <h1>NIEUWSTE BEORDERLINGEN</h1>
         </div>
 
     </body>
