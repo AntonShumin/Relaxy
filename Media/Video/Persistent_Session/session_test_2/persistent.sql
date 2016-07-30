9f95fda5ea152a6c01942c1966c96284a319091b
AntonShumin <AntonShuminBe@gmail.com>
2016-07-30 14:40:19 +0200

    check db connection

--- Media/Video/Persistent_Session/session_test_2/persistent.sql
+++ Media/Video/Persistent_Session/session_test_2/persistent.sql
@@ -1,20 +1,24 @@
-CREATE TABLE IF NOT EXISTS 'users' (
-  'user_key' char(8) NOT NULL PRIMARY KEY,
-  'username' varchar(30) NOT NULL UNIQUE,
-  'pwd' varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_key` char(8) NOT NULL PRIMARY KEY,
+  `username` varchar(30) NOT NULL UNIQUE,
+  `pwd` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS 'sessions' (
-  'sid' varchar(40) NOT NULL PRIMARY KEY,
-  'expiry' int(10) unsigned NOT NULL,
-  'data' text NOT NULL
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `sid` varchar(40) NOT NULL PRIMARY KEY,
+  `expiry` int(10) unsigned NOT NULL,
+  `data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS 'autologin' (
-  'user_key' CHAR(8)    NOT NULL,
-  'token'    CHAR(22)   NOT NULL,
-  'create'   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  'data'     TEXT,
-  'used'     TINYINT(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY ('user_key', 'token')
+
+CREATE TABLE IF NOT EXISTS `autologin` (
+  `user_key` char(8) NOT NULL,
+  `token` char(32) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` text,
+  `used` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_key`, `token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Andere engine kan MyISAM zijn
