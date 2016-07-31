95aa0656681fce836bbcba16da8a936c3c5a0b42
AntonShumin <AntonShuminBe@gmail.com>
2016-07-31 11:12:35 +0200

    session read method

--- Media/Video/Persistent_Session/Foundationphp/Sessions/session_handler.php
+++ Media/Video/Persistent_Session/Foundationphp/Sessions/session_handler.php
@@ -22,11 +22,42 @@ class MysqlSessionHandler implements \SessionHandlerInterface {
     }
 
     public function open($save_path,$name) {
-
+        return true;
     }
 
     public function read($session_id) {
-
+        try {
+            if ($this->useTransactions) {
+                $this->db->exec('SET TRANSACTION ISOLATION LEVEL READ COMMITTED');
+                $this->db->beginTransaction();
+            } else {
+                $this->unlockStatements[] = $this->getLock($session_id);
+            }
+            $sql =  "SELECT $this->col_expiry,$this->col_data
+                    FROM $this->$this->table_sess WHERE $this->col_sid = :sid";
+            if ($this->useTransactions) {
+                $sql .= ' FOR UPDATE';
+            }
+            $selectStmt = $this->db->prepare($sql);
+            $selectStmt->bindParam(':sid',$session_id);
+            $selectStmt->execute();
+            $results = $selectStmt->fetch(\PDO::FETCH_ASSOC);
+            if ($results) {
+                if($results[$this->col_expiry] <time()) {
+                    return '';
+                }
+                return $results[$this->col_data];
+            }
+            if($this->useTransactions) {
+                $this->initializeRecord($selectStmt);
+            }
+            return '';
+        } catch (\PDOException $e) {
+            if ($this->db->inTransaction()) {
+                $this->db->rollBack();
+            }
+            throw $e;
+        }
     }
 
     public function write($session_id, $data) {
